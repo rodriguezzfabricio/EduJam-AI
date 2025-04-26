804179a306720a1cc6b1517533448f03d5d78977
rodriguezzfabricio <frodrig3@umbc.edu>
2025-04-26 11:33:04 -0400

    Initial project setup

---
+++ src/main/java/EduJam/AI/EduJam/AI/EduJamAiApplication.java
@@ -0,0 +1,13 @@
+package EduJam.AI.EduJam.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduJamAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduJamAiApplication.class, args);
+	}
+
+}
