fd8b128ed1e95de0a53c6210319ece22672f6906
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-04-26 14:17:10 -0400

    fixed structure

--- backend/src/main/java/EduJam/AI/EduJamAiApplication.java
+++ backend/src/main/java/EduJam/AI/EduJamAiApplication.java
@@ -1,1 +1,11 @@
 package EduJam.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduJamAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduJamAiApplication.class, args);
+    }
+}
